e48eafabb60e146da5901745b63ff9d4c30ee5e9
hcasten <hcasten@outlook.com>
2023-10-03 23:29:12 -0400

    test cleanup

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,59 +1,41 @@
-use anyhow::Result;
-use axum::Router;
-
-use api::{auth::create_oauth_client, sessions::SessionManager, configuration::{AuthSettings, Settings}};
+use api::app::create_router;
+use api::auth::create_oauth_client;
+use api::configuration::Settings;
+use api::database::PhotoRepository;
+use api::domain::AppState;
+use api::sessions::SessionManager;
 use aws_sdk_s3::config::Region;
 use aws_sdk_s3::Client;
 
-use api::app::create_router;
-use api::database::PhotoRepository;
-use api::domain::AppState;
-use async_once::AsyncOnce;
-use lazy_static::lazy_static;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
-use std::net::SocketAddr;
-use tokio::runtime::Runtime;
 use utoipa_swagger_ui::{Config, SwaggerUi};
 
-lazy_static! {
-    static ref RUNTIME: Runtime = Runtime::new().unwrap();
+use std::net::SocketAddr;
+
+use hyper::server::Server;
+
+use tokio::sync::OnceCell;
+
+static SERVER: OnceCell<SocketAddr> = OnceCell::const_new();
+
+pub(crate) async fn get_or_init_server() -> &'static SocketAddr {
+    SERVER.get_or_init(spawn_server).await
 }
 
-lazy_static! {
-    pub static ref API: AsyncOnce<()> = AsyncOnce::new(async {
-        std::env::set_var("RUN_MODE", "test");
+async fn spawn_server() -> SocketAddr {
+    let settings = Settings::load_config().unwrap();
 
-        let app = spawn_app().await.expect("Unable to start app");
-        let address = SocketAddr::from(([127, 0, 0, 1], 6969));
-
-        tokio::spawn(async move {
-            axum::Server::bind(&address)
-                .serve(app.into_make_service())
-                .await
-                .expect("Failed to start server");
-        });
-    });
-}
-
-pub async fn spawn_app() -> Result<Router> {
-    let settings = Settings::load_config()?;
-
-    let aws_endpoint_url = std::env::var("AWS_ENDPOINT_URL").expect("AWS_ENDPOINT_URL must be set");
     let _aws_access_key =
         std::env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID must be set");
     let _aws_secret_key =
         std::env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY must be set");
-    let _aws_bucket_name = std::env::var("AWS_BUCKET_NAME").expect("AWS_BUCKET_NAME must be set");
-    let aws_region = std::env::var("AWS_REGION").expect("AWS_REGION must be set");
 
     let config = aws_config::from_env()
-        .endpoint_url(aws_endpoint_url)
-        .region(Region::new(aws_region))
+        .endpoint_url(&settings.aws_endpoint_url)
+        .region(Region::new(settings.aws_region.clone().to_owned()))
         .load()
         .await;
-
-
 
     let client = Client::new(&config);
     let pool: PgPool = PgPoolOptions::new()
@@ -62,28 +44,28 @@ pub async fn spawn_app() -> Result<Router> {
         .await
         .expect("can't connect to database");
 
-    sqlx::migrate!().run(&pool).await.unwrap();
     let photo_repo = PhotoRepository::new(pool.clone());
 
+    photo_repo.migrate().await.unwrap();
+
     let oauth_client = create_oauth_client(settings.auth_settings).unwrap();
-    let redis =
-        redis::Client::open(std::env::var("REDIS_URL").expect("REDIS_URL must be set")).unwrap();
-    let session_manager = SessionManager::new(redis);
+
+    let session_manager = SessionManager::new(redis::Client::open(settings.redis_url).unwrap());
     let app_state = AppState::new(photo_repo, oauth_client, client, session_manager);
 
     let swagger_ui = SwaggerUi::new("/swagger-ui")
         .config(Config::from("/api/enchanted-natures.openapi.spec.yaml"));
     let app = create_router(swagger_ui, app_state);
-    Ok(app)
-}
 
-pub fn use_app<F>(test: F)
-where
-    F: std::future::Future,
-{
-    RUNTIME.block_on(async move {
-        API.get().await;
+    let addr = SocketAddr::from(([127, 0, 0, 1], 0));
 
-        test.await;
-    })
+    let server = Server::bind(&addr).serve(app.into_make_service());
+    let actual_addr = server.local_addr();
+
+    // Spawn server task
+    tokio::spawn(async move {
+        server.await.expect("Server task failed");
+    });
+
+    actual_addr
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,47 +1,83 @@
-use crate::common::*;
+use crate::common::get_or_init_server;
 use api::models::PhotoViewModel;
+use hyper::{Body, Client, Method, Request, Uri};
 
 mod common;
 
-#[test]
-fn health_check() {
-    use_app(async {
-        let response = reqwest::get("http://localhost:6969/health_check")
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
+#[tokio::test]
+async fn health_check() {
+    let addr = get_or_init_server().await;
+    let client = Client::new();
+    let uri: Uri = format!("http://{}/health_check", addr).parse().unwrap();
 
-        assert_eq!(&response[..], "{\"status\":\"Ok\"}");
-    })
+    // Construct JSON payload to create user.
+    // Create request.
+    let req = Request::builder()
+        .method(Method::GET)
+        .uri(uri)
+        .body(Body::from(""))
+        .expect("request builder");
+
+    let resp = client.request(req).await.expect("request failed");
+
+    // Further assertions based on your API spec, e.g., checking the response JSON.
+    assert_eq!(resp.status(), 200);
+    assert_eq!(
+        resp.headers().get("content-type").unwrap(),
+        "application/json"
+    );
+    // assert the status is ok
+    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    assert_eq!("{\"status\":\"Ok\"}", body);
 }
 
-#[test]
-fn photos() {
-    use_app(async {
-        let response = reqwest::get("http://localhost:6969/api/v0/photos")
-            .await
-            .unwrap()
-            .json::<Vec<PhotoViewModel>>()
-            .await
-            .unwrap();
+#[tokio::test]
+async fn photos() {
+    let addr = get_or_init_server().await;
+    let client = Client::new();
+    let uri: Uri = format!("http://{}/api/v0/photos", addr).parse().unwrap();
 
-        assert!(!response.is_empty())
-    })
+    let req = Request::builder()
+        .method(Method::GET)
+        .uri(uri)
+        .body(Body::from(""))
+        .expect("request builder");
+
+    let resp = client.request(req).await.expect("request failed");
+
+    // Ensure status code is 200 OK
+    assert_eq!(resp.status(), 200);
+    // Ensure content-type is application/json
+    assert_eq!(
+        resp.headers().get("content-type").unwrap(),
+        "application/json"
+    );
+
+    // Deserialize the JSON body
+    let body_bytes = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    let photos: Vec<PhotoViewModel> =
+        serde_json::from_slice(&body_bytes).expect("Failed to deserialize JSON");
+
+    // Additional assertions based on your API spec and test criteria
+    assert!(!photos.is_empty());
 }
 
-#[test]
-fn auth() {
-    use_app(async {
-        let response = reqwest::get("http://localhost:6969/authorize")
-            .await
-            .unwrap();
+#[tokio::test]
+async fn auth() {
+    let addr = get_or_init_server().await;
+    let client = Client::new();
+    let uri: Uri = format!("http://{}/authorize", addr).parse().unwrap();
 
-        assert!(response
-            .url()
-            .to_string()
-            .contains("auth.enchantednatures.com"))
-        // assert!(response.len() > 0)
-    })
+    // Construct JSON payload to create user.
+    // Create request.
+    let req = Request::builder()
+        .method(Method::GET)
+        .uri(uri)
+        .body(Body::from(""))
+        .expect("request builder");
+
+    let resp = client.request(req).await.expect("request failed");
+
+    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    assert_eq!("", body);
 }
